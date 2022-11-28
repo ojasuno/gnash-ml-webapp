0605bc1cf0b2f793f93e19ce717fa1d18a4ba60f
ojasuno <ojasuno21@gmail.com>
2022-11-28 18:16:42 +1100

    openAIAPI

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,43 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Lyrics Writer | CyberBrokers | buildspace | GPT-3 | ML</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>GNASH-ML</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>generate lyrics for your CyberBroker</h2>
+          </div>
+        </div>
+        {/* new bits about a prompt container */}
+        <div className="prompt-container">
+          <textarea 
+            className="promp-box" 
+            placeholder="type here..." 
+            value={userInput}
+            onChange={onUserChangedText}
+          />;
+          {/* new bits about a button */}
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
@@ -26,7 +49,7 @@ const Home = () => {
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>built w/ buildspace</p>
           </div>
         </a>
       </div>
