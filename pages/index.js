24732a25101ba5909ce8adb457e940f2feb71e3a
ojasuno <ojasuno21@gmail.com>
2022-11-29 22:27:29 +1100

    logo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import ojasLogo from '../assets/ojas-hr-logo.png';
 import { useState } from 'react';
 
 const Home = () => {
@@ -82,13 +82,13 @@ const Home = () => {
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://github.com/ojasuno/gnash-ml-webapp"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>built w/ buildspace</p>
+            <Image src={ojasLogo} alt="ojas logo" />
+            <p>❤️ ojas ❤️</p>
           </div>
         </a>
       </div>
