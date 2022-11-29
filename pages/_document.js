95452105663dc86bb17090b59cfa319b6eecca8a
ojasuno <ojasuno21@gmail.com>
2022-11-29 23:05:52 +1100

    share image

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="GNASH ML" key="title"/>
+        <meta property="og:description" content="generate lyrics for your CyberBroker" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="../assets/gnash-ml.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>GNASH-ML</h1>
+            <h1>GNASH ML</h1>
           </div>
           <div className="header-subtitle">
             <h2>generate lyrics for your CyberBroker</h2>
